4c242e400891ada506b5d4f38baba6a78c15de25
RYAN PILON <Ryanpilon1995@gmail.com>
2020-08-19 19:59:06 -0700

    created css and js

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict'
+
+
+
+var userName = prompt('whats your name?');
+
+
+if (userName === "GOD") {
+    document.write('<h1>' + 'WELCOME CREATOR ' + userName + '</h1>');
+}
+else {
+    document.write('<h1>' + 'Welcome ' + userName + '</h1>');
+}
+var dogbreed = prompt('Whats your favorite breed of dog?');
+
+if (dogbreed === "black lab"){
+    alert(userName + ' Black labs are the best!');
+} 
+else {
+    alert(userName + ' well i like black labs');
+}
+
+var dogbone = prompt('What dog keeps the best time?');
+
+if (dogbone === "a watch dog"){
+    alert(userName+ "how did you guess that?");
+}
+else {
+    alert(username + ", A Watch Dog");
+}
